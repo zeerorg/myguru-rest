7fd74ac806376de7d3d770ae9f1a91557f1c6073
Rishabh Gupta <rishabh4325@gmil.com>
2017-09-10 18:16:21 +0800

    Auth token request with tests

--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,9 @@ import os
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth.models import User
+from rest_framework.authtoken import views as auth_views
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIRequestFactory
 
 from main import views
 from rest import settings
@@ -54,5 +57,59 @@ class TeacherSaveTest(TestCase):
         new_data["name"] = "Rishabh Gupta New"
         self.assertEqual(views.save_teacher(new_data), True)
         self.assertEqual(models.Teacher.objects.get(email=new_data["email"]).phone, new_data["phone"])
+
+
+class TestStudentAuthRequest(TestCase):
+    def setUp(self):
+        with open(os.path.join(settings.BASE_DIR, "test_dir", "images.jpe"), 'rb') as content_file:
+            content = content_file.read()
+        self.data = {
+            "name": "Rishabh Gupta",
+            "phone": "9988776622",
+            "email": "randomail3@outlook.com",
+            "study_year": "11",
+            "profile_pic": SimpleUploadedFile("yo.jpg", content, content_type="images/jpeg"),
+        }
+        self.student = models.Student.objects.create(**self.data)
+        self.student.save()
+        self.data["password"] = "12345678"
+        self.user = User.objects.create_user(self.data['email'], self.data['email'], self.data["password"])
+        self.token = Token.objects.create(user=self.user)
+        self.factory = APIRequestFactory()
+
+    def testAuthToken(self):
+        request = self.factory.post("/api-token-auth/", {"username": self.data["email"], "password": self.data["password"]})
+        response = auth_views.obtain_auth_token(request)
+        self.assertEqual(response.data["token"], self.token.key)
+
+
+class TestTeacherAuthToken(TestCase):
+    def setUp(self):
+        with open(os.path.join(settings.BASE_DIR, "test_dir", "images.jpe"), 'rb') as content_file:
+            content = content_file.read()
+        self.data = {
+            "name": "Rishabh Gupta",
+            "phone": "9988776655",
+            "email": "randomail4@outlook.com",
+            "about": "I teach Physics",
+            "profile_pic": SimpleUploadedFile("yo.jpg", content, content_type="images/jpeg")
+        }
+        self.teacher = models.Teacher.objects.create(**self.data)
+        self.teacher.save()
+        self.data["password"] = "12345678"
+        self.user = User.objects.create_user(self.data['email'], self.data['email'], self.data["password"])
+        self.token = Token.objects.create(user=self.user)
+        self.factory = APIRequestFactory()
+
+    def testAuthToken(self):
+        request = self.factory.post("/api-token-auth/", {"username": self.data["email"], "password": self.data["password"]})
+        response = auth_views.obtain_auth_token(request)
+        self.assertEqual(response.data["token"], self.token.key)
+
+
+
+
+
+
 
 
--- rest/urls.py
+++ rest/urls.py
@@ -15,15 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from main import views as main_views
 from django.conf.urls.static import static
+from rest_framework.authtoken import views as auth_views
+
+from main import views as main_views
 from rest import settings
 
 urlpatterns = [
     url(r'^register/student', main_views.register_student),
     url(r'^register/teacher', main_views.register_teacher),
+    url(r'^api-token-auth/', auth_views.obtain_auth_token),
     url(r'^admin/', admin.site.urls),
     url(r'^', main_views.main_page),
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
